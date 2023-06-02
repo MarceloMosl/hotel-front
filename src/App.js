451b0d748ec47909c8869c283017487937cc30a3
MarceloMosl <marcelomosl@icloud.com>
2023-06-01 21:31:39 -0300

    Feat: inital

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 function App() {
